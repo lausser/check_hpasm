52ea00e02a488bfea3038a6be9916717b1338dba
Gerhard Lausser <gerhard.lausser@consol.de>
2014-03-20 01:16:22 +0100

    add StoreEasy detection

--- plugins-scripts/HP/Server.pm
+++ plugins-scripts/HP/Server.pm
@@ -49,6 +49,9 @@ sub new {
       } elsif ($self->{productname} =~ /StorageWorks/i) {
         bless $self, 'HP::StorageWorks';
         $self->trace(3, 'using HP::StorageWorks');
+      } elsif ($self->{productname} =~ /StoreEasy/i) {
+        bless $self, 'HP::Proliant::SNMP';
+        $self->trace(3, 'using HP::Proliant::SNMP');
       } elsif ($self->{productname} =~ /Storage/) { # fake
         bless $self, 'HP::Storage';
         $self->trace(3, 'using HP::Storage');
